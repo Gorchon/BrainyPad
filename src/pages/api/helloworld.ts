8e4f723aa8de5f4bff9fcdc12ad03959148af1c9
DanielBrMz <a01254805@tec.mx>
2024-04-30 12:00:00 -0600

    git deploy action set

--- src/pages/api/helloworld.ts
+++ src/pages/api/helloworld.ts
@@ -4,7 +4,7 @@ import { allUsers } from "../../server/queries/getAllUsers";
 export const GET: APIRoute = ({ request }) => {
   return new Response(
     JSON.stringify({
-      message: "Que pedo genteeeeeeeeeee, Todos con las manos en el aire",
+      message: "Que pedo genteeeeeeeeeee. Todos con las manos en el aire",
       users: allUsers,
     }),
     {status:200,  headers: { "content-type": "application/json" } }
