305fa2af37436abebde796153e22b731d61ea0f2
Gorchon <A01254831@tec.mx>
2024-05-02 16:50:55 -0600

    Sidebar done

--- src/components/react/Sidebar.tsx
+++ src/components/react/Sidebar.tsx
@@ -1,58 +1,68 @@
-import { User, Settings } from "lucide-react";
+import {
+  User,
+  Settings,
+  Home,
+  Search,
+  MessageCircleMore,
+  Folders,
+} from "lucide-react";
 
 const Sidebar = () => {
   return (
     <>
       <div
-        className="w-80 bg-gray-200 shadow-md overflow-auto z-10"
+        className="flex flex-col w-80 bg-gray-200 shadow-md overflow-hidden z-10"
         style={{ height: "100vh" }}
       >
-        <div className="p-5 font-bold text-5xl text gray-">BrainyPad</div>
-        <ul className="space-y-2 p-5">
+        <div className="p-5 font-bold text-5xl">BrainyPad</div>
+        <ul className="flex-grow overflow-auto space-y-8 p-8 text-xl">
+          {" "}
           <li>
             <a
               href="/home"
-              className="flex items-center space-x-2 text-gray-700 hover:text-blue-500"
+              className="flex items-center space-x-3 text-gray-700 hover:text-blue-500" // Aumentado espacio
             >
-              <span>🏠</span>
+              <Home size={24} />
               <span>Home</span>
             </a>
           </li>
           <li>
             <a
               href="/search"
-              className="flex items-center space-x-2 text-gray-700 hover:text-blue-500"
+              className="flex items-center space-x-3 text-gray-700 hover:text-blue-500"
             >
-              <span>🔍</span>
+              <Search size={24} />
               <span>Search</span>
             </a>
           </li>
           <li>
             <a
               href="/chat"
-              className="flex items-center space-x-2 text-gray-700 hover:text-blue-500"
+              className="flex items-center space-x-3 text-gray-700 hover:text-blue-500"
             >
-              <span>💬</span>
+              <MessageCircleMore size={24} />
               <span>Chat</span>
             </a>
           </li>
           <li>
             <a
               href="/files"
-              className="flex items-center space-x-2 text-gray-700 hover:text-blue-500"
+              className="flex items-center space-x-3 text-gray-700 hover:text-blue-500"
             >
-              <span>📁</span>
+              <Folders size={24} />
               <span>Files</span>
             </a>
           </li>
         </ul>
-        <div className="px-5 py-2 border-t mt-auto">
-          <div className="flex items-center space-x-3">
-            <button className="z-10 ml-[2rem] flex h-14 w-14 items-center justify-center rounded-full bg-gray-300 outline-1 outline-white hover:outline">
-              <User className="h-6 w-6 stroke-dark" />
+        <div className="px-5 py-2 border-t w-full text-xl">
+          {" "}
+          <div className="flex items-center justify-between">
+            <button className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-300">
+              {" "}
+              <User size={24} className="stroke-dark" />{" "}
             </button>
             <span className="text-gray-800">User Name</span>
-            <Settings className="h-6 w-6 stroke-dark" />
+            <Settings size={24} className="stroke-dark" />{" "}
           </div>
         </div>
       </div>
