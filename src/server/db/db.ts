17a6742c30f52946be2989c9544f8f9d3d1c10f9
DanielBrMz <a01254805@tec.mx>
2024-05-03 02:07:52 -0600

    done

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -2,11 +2,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
+/* if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing");
 }
-
-const connectionString = process.env.DATABASE_URL;
+ */
+const connectionString = "postgres://postgres.cqqmxosfafldprwsbank:[nTH6YCqUqZiaLMaaVxuyCn8YuZjgL4hz]@aws-0-us-west-1.pooler.supabase.com:5432/postgres";
 const client = postgres(connectionString);
 const db = drizzle(client, { schema });
 
--- src/server/db/queryExample.ts
+++ src/server/db/queryExample.ts
@@ -1,14 +1,17 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { users } from './schema'
+import 'dotenv/config'
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing");
 }
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL;;
 
-const client = postgres(connectionString)
+const client = postgres(connectionString, { prepare: false })
 const db = drizzle(client);
 
 const allUsers = await db.select().from(users);
+
+console.log(allUsers);
