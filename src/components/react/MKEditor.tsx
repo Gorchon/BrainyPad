ae015e461c3c878ea4b4d5b72c8404eeee672356
Y0z64 <yairprogrammer@gmail.com>
2024-05-02 19:36:51 -0600

    Added markdown editor

---
+++ src/components/react/MKEditor.tsx
@@ -0,0 +1,142 @@
+import {
+  AdmonitionDirectiveDescriptor,
+  BoldItalicUnderlineToggles,
+  ChangeCodeMirrorLanguage,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  CodeToggle,
+  ConditionalContents,
+  CreateLink,
+  directivesPlugin,
+  frontmatterPlugin,
+  imagePlugin,
+  InsertAdmonition,
+  InsertCodeBlock,
+  InsertSandpack,
+  InsertTable,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  ListsToggle,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin,
+  sandpackPlugin,
+  ShowSandpackInfo,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+  type SandpackConfig,
+} from "@mdxeditor/editor";
+import { headingsPlugin } from "@mdxeditor/editor";
+
+import "@mdxeditor/editor/style.css";
+
+export default function MKEditor() {
+  const markdown = `
+    # Something
+    ## Something else
+`;
+
+  async function imageUploadHandler(image: File) {
+    const formData = new FormData();
+    formData.append("image", image);
+    // send the file to your server and return
+    // the URL of the uploaded image in the response
+    const response = await fetch("/uploads/new", {
+      method: "POST",
+      body: formData,
+    });
+    const json = (await response.json()) as { url: string };
+    return json.url;
+  }
+
+  const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`.trim();
+
+  const simpleSandpackConfig: SandpackConfig = {
+    defaultPreset: "react",
+    presets: [
+      {
+        label: "React",
+        name: "react",
+        meta: "live react",
+        sandpackTemplate: "react",
+        sandpackTheme: "light",
+        snippetFileName: "/App.js",
+        snippetLanguage: "jsx",
+        initialSnippetContent: defaultSnippetContent,
+      },
+    ],
+  };
+
+  return (
+    <MDXEditor
+      markdown={markdown}
+      className="outline-orange-400 outline w-full"
+      contentEditableClassName="prose"
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        frontmatterPlugin(),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              {" "}
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <InsertAdmonition />
+              <CreateLink />
+              <CodeToggle />
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editor) => editor?.editorType === "codeblock",
+                    contents: () => <ChangeCodeMirrorLanguage />,
+                  },
+                  {
+                    when: (editor) => editor?.editorType === "sandpack",
+                    contents: () => <ShowSandpackInfo />,
+                  },
+                  {
+                    fallback: () => (
+                      <>
+                        <InsertCodeBlock />
+                        <InsertSandpack />
+                      </>
+                    ),
+                  },
+                ]}
+              />
+              <ListsToggle />
+              <InsertTable />
+            </>
+          ),
+        }),
+        linkDialogPlugin(),
+        imagePlugin({ imageUploadHandler }),
+        directivesPlugin({
+          directiveDescriptors: [AdmonitionDirectiveDescriptor],
+        }),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+        sandpackPlugin({ sandpackConfig: simpleSandpackConfig }),
+        codeMirrorPlugin({
+          codeBlockLanguages: { js: "JavaScript", css: "CSS" },
+        }),
+      ]}
+    />
+  );
+}
