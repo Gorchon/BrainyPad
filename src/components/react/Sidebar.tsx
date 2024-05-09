ff1add33d4defaf06b72fe9bafe61aa1ce13910b
HEmilGS <hectoremil748@gmail.com>
2024-05-09 17:41:48 -0600

    changed icons

--- src/components/react/Sidebar.tsx
+++ src/components/react/Sidebar.tsx
@@ -5,6 +5,8 @@ import {
   Search,
   MessageCircleMore,
   Folders,
+  SunMedium,
+  Moon,
 } from "lucide-react";
 
 import { UserButton } from "@clerk/clerk-react";
@@ -93,10 +95,12 @@ const handleChangeTheme = () => {
 
         <label className="inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" className="sr-only peer" />
-          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Light</span>
-          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+          <span className="ms-3"></span>
+          <SunMedium size={30} className="dark:stroke-white"/>
+          <span className="ms-3"></span>
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 outline outline-1 outline-gray-900" onClick={handleChangeTheme}></div>
-          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">dark</span>
+          <span className="ms-3"></span>
+          <Moon size={25} className="dark:stroke-white"/>
         </label>
 
         <br />
