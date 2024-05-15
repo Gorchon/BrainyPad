1b3589c4f41a72e4fbed908c70ae733c5333284b
Y0z64 <yairprogrammer@gmail.com>
2024-05-14 19:44:15 -0600

    Removed bloat, added bee movie

--- src/env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="astro/client" />
-/// <reference types="astro-clerk-auth/env" />
--- src/pages/api/status.ts
+++
@@ -1,5 +0,0 @@
-export function GET(_req: Request) {
-  return new Response(JSON.stringify({ status: "ok" }), {
-    headers: { "content-type": "application/json" },
-  });
-}
