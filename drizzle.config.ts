cc5d454da7cb2a44c815ac5996766b352d033929
Axel Padilla <adpadillar25@gmail.com>
2024-05-31 14:36:19 -0600

    chat with document endpoint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing");
 }
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
-} satisfies Config;
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
